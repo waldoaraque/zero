5349c8ce1fbdd2ea5da880041dfdf3a81f2a5e39
waldoaraque <waldoaraque10@gmail.com>
2019-01-16 15:08:10 -0400

    Basic practice to print with printf() method differents vars

---
+++ go/printf.go
@@ -0,0 +1,15 @@
+package main 
+import (
+	"fmt"
+)
+
+func main(){
+	name := "Waldo"
+	age := 20
+	status := true
+	average := 6.2
+	fmt.Printf("My name is: %v\n", name)
+	fmt.Printf("My age is: %d\n", age)
+	fmt.Printf("Developer status: %t\n", status)
+	fmt.Printf("Developer average: %f\n", average)
+}
