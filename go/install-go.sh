46c3685e27bfa356fbd36df9e73e8759ab955280
waldoaraque <waldoaraque10@gmail.com>
2019-01-16 16:20:58 -0400

    Adding practice for read and print data in go

---
+++ go/install-go.sh
@@ -0,0 +1,2 @@
+sudo apt-get update && sudo apt-get -y upgrade
+curl -O https://storage.googleapis.com/golang/go1.11.2.linux-amd64.tar.gz
---
+++ go/intput-output.go
@@ -0,0 +1,11 @@
+package main 
+import(
+	"fmt"
+)
+
+func main() {
+	var age int
+	fmt.Println("Enter your age: ")
+	fmt.Scanf("%d\n",&age)
+	fmt.Printf("My age is %d\n", age)	
+}
