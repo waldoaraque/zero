cdf1917042276db490d2ae90f5d759e6ff2e02f5
waldoaraque <waldoaraque10@gmail.com>
2019-01-16 14:42:44 -0400

    Adding the go workflow with their vars

---
+++ go/vars.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main() {
+	//var name type
+	//var number int // start in 0
+	//var letter string // start in empty
+	//var logical bool // start in false
+	//var vector []string //string characters
+
+	//x := 23 //assign a value without declaring the type
+	name := "Waldo"
+	fmt.Println(name)
+}
