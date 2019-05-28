72f92d4eee0bcac2c0cdbf283b6cc3daa2e4d8f9
waldoaraque <waldoaraque10@gmail.com>
2019-05-28 09:52:54 -0400

    Working with callbacks, promises -> .then()

--- nodejs/callbacks-promises/index.js
+++ nodejs/callbacks-promises/index.js
@@ -1,1 +1,8 @@
 const call = require('./src/call');
+
+console.log(call.sync('Sync Function'));
+
+call.withCallback('CallBack Function', call.sync);
+
+call.withPromise('Promise Function')
+  .then( name => console.log(name) );
--- nodejs/callbacks-promises/src/call.js
+++ nodejs/callbacks-promises/src/call.js
@@ -1,15 +1,15 @@
 module.exports = {
     sync: function(nombre) {
-        return `Hola ${nombre}! Bienvenido a Node.js`;
+        return `Hello ${nombre}! with Node.js`;
     },
     withCallback: function(nombre, cb) {
-        setTimeout(()=>{
-            cb(nombre)
+        setTimeout( () => {
+            console.log(cb(nombre));
         }, 5000);
     },
     withPromise: function(nombre) {
-        return new Promise((resolve, reject)=>{
-            setTimeout(()=>{
+        return new Promise((resolve, reject) => {
+            setTimeout( () => {
                 resolve(nombre);
             }, 5000);
         });
