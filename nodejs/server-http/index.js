6499f0130518be1d7df34ab6d1b7407f99510269
waldoaraque <waldoaraque10@gmail.com>
2019-05-29 09:31:07 -0400

    Creating a basic server with node.js native http module

---
+++ nodejs/server-http/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http
+  .createServer( (req, res) => {
+  	fs.readFile("./resources/index.html", (err, data) => {
+  		if (err) {
+        console.error(err);
+        return;
+      }
+  		res.end(data)
+  	});
+  });
+
+server.listen(3000);
