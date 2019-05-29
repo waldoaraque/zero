b75513389a4c2aa71b6e30f83a03e13acbd2c9fa
waldoaraque <waldoaraque10@gmail.com>
2019-05-29 10:27:36 -0400

    working with APIs in node.js + mongodb

--- nodejs/api-express/index.js
+++ nodejs/api-express/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const { PORT } = require("./config");
 
-require("./routes/views")(app);
 require("./routes/api")(app);
+require("./routes/views")(app);
 
 function init(){
-    console.log("Iniciando instancia de Express...");
+    console.log("Stargint express instance...");
     app.listen(PORT, ()=>{
-        console.log("El servidor Express esta activo.");
+        console.log("Express server is running.");
     });
 }
 
