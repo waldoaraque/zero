34dadfc9a6b80438fe5718adb57ef057e822735d
waldoaraque <waldoaraque10@gmail.com>
2019-06-04 08:05:12 -0400

    creating my api from an public api

--- node.js/express-routes/index.js
+++ node.js/express-routes/index.js
@@ -4,11 +4,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use( logger('dev') );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ extended: false }) );
 
 require('./routes/views')(app);
+require('./routes(api')(app);
 require('./routes/special')(app);
 
 console.log("Iniciando Express.js");
