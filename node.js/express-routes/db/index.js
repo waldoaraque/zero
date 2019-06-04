12bd529523e99a7845a423b33b8c753be5ab62aa
waldoaraque <waldoaraque10@gmail.com>
2019-06-04 09:11:54 -0400

    adding function getFilms() and getFilmById()

--- node.js/express-routes/db/index.js
+++ node.js/express-routes/db/index.js
@@ -2,6 +2,16 @@ const mongo = require("./connect");
 const { DB_NAME } = require("./config");
 
 module.exports = {
+    getFilms: function() {
+        const db = mongo.instance().db(DB_NAME);
+        const resp = db.collection("films").find().toArray();
+        return resp;
+    },
+    getFilmByID: function(id){
+        const db = mongo.instance().db(DB_NAME);
+        const resp = db.collection("films").find({ id }).toArray();
+        return resp;
+    },
     postFilm: function(film) {
         const db = mongo.instance().db(DB_NAME);
         const resp = db.collection("films").insertOne(film);
