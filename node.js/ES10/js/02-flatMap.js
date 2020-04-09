34ffdabd63296bbcaf1948e51bcbf640eceb0044
waldev <waldoaraque10@gmail.com>
2020-04-09 22:05:37 +0200

    working with flatMap() method

--- node.js/ES10/js/02-flatMap.js
+++ node.js/ES10/js/02-flatMap.js
@@ -2,4 +2,10 @@ const productos = [
     { nombre: 'Producto 1', precio: 20},
     { nombre: 'Producto 2', precio: 30},
     { nombre: 'Producto 3', precio: 40}
-];
+]
+
+//flatMap() => mapea un elemento y retorna un nuevo arreglo
+
+const resultado = productos.flatMap( producto => [producto.nombre, producto.precio])
+
+console.log(resultado)
