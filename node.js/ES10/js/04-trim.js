047025060de7af73951dff7f7088400c72b28873
waldev <waldoaraque10@gmail.com>
2020-04-09 22:13:28 +0200

    working with trim in console

--- node.js/ES10/js/04-trim.js
+++ node.js/ES10/js/04-trim.js
@@ -1,1 +1,11 @@
 const correo = '    correo@correo.com    ';
+
+//trimStart() => permite eliminar el espacio en blanco de una cadena de texto
+
+console.log(correo.trimStart())
+
+console.log(correo.trimEnd())
+
+console.log(correo.trimStart().trimEnd())
+
+console.log(correo.trim())
