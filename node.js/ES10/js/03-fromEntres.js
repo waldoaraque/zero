f4ae6455e6e7425a299646f81b8eef3c361b4c83
waldev <waldoaraque10@gmail.com>
2020-04-09 22:10:04 +0200

    working with fromEntries() with an Map() object

--- node.js/ES10/js/03-fromEntres.js
+++ node.js/ES10/js/03-fromEntres.js
@@ -1,4 +1,14 @@
 const map = new Map([ 
     ['nombre', 'Producto 1'], 
     ['precio', 20] 
-]);
+])
+
+//fromEntries() => permite crear un objeto de una serie de llaves y valores
+
+const objeto = Object.fromEntries(map)
+console.log(objeto)
+
+//se pueden utilizar metodos la entidad Object
+delete objeto.precio
+
+console.log(objeto)
